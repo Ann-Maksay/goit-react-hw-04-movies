adbe284401bfcc599602ef34ff8d31944d07c0ba
Ann Maksay <annaorlova.dn@gmail.com>
2021-06-14 18:30:53 +0300

    исправила пути

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const routes = {
-  home: "/",
-  movies: "/movies",
-  moviesDetails: "/movies/:movieId",
+  home: "/goit-react-hw-04-movies/",
+  movies: "/goit-react-hw-04-movies/movies",
+  moviesDetails: "/goit-react-hw-04-movies/movies/:movieId",
 };
 
 export default routes;

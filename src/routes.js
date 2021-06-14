22a9aad5155856cf684606c39e3c0739c0add7a9
Ann Maksay <annaorlova.dn@gmail.com>
2021-06-14 18:33:05 +0300

    вернула назад

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const routes = {
-  home: "/goit-react-hw-04-movies/",
-  movies: "/goit-react-hw-04-movies/movies",
-  moviesDetails: "/goit-react-hw-04-movies/movies/:movieId",
+  home: "/",
+  movies: "/movies",
+  moviesDetails: "/movies/:movieId",
 };
 
 export default routes;
